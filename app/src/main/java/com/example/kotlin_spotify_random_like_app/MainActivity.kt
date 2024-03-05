15db76d12766001eade3f296fe3f607e1810c38b
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-05 13:56:55 +0300

    viewBinding

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
@@ -15,20 +15,25 @@ import com.example.kotlin_spotify_random_like_app.databinding.ActivityMainBindin
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val clientId = "1e6d0591bbb64af286b323ff7d26ce0f"
-    private val redirectUri = "http://com.example.kotlin_spotify_random_like_app/callback"
+    private val redirectUri = "http://com.example.kotlin_spotify_random_like_app://callback"
     private var spotifyAppRemote: SpotifyAppRemote? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Log.e("Baglandi", "burada")
+        binding.button.setOnClickListener{
+
+        }
     }
     override fun onStart() {
         super.onStart()
+        Log.e("Baglandi", "heyyy")
         val connectionParams = ConnectionParams.Builder(clientId)
             .setRedirectUri(redirectUri)
             .showAuthView(true)
             .build()
-
+        Log.e("Baglandi", "heyyy2")
         SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 Log.e("Baglandi", "burada")
