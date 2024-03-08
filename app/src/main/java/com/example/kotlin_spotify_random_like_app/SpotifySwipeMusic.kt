8a503043c91e504e22c81480953150a8c20869ec
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-08 11:34:25 +0300

    Added Swipe Activity

---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifySwipeMusic.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spotify_random_like_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpotifySwipeMusic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spotify_swipe_music)
+    }
+}
