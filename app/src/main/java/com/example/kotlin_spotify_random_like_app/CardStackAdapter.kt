1aff54059f35c1ade35f2bf3e60ffcb69ba2cd56
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-08 15:52:20 +0300

    Added ItemModel Class

---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/CardStackAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_spotify_random_like_app
+
+class CardStackAdapter {
+}
---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/ItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin_spotify_random_like_app
+
+class ItemModel(image: Int,name:String, usia: String, kota:String ) {
+    private var image: Int = 0
+    private lateinit var name:String
+    private lateinit var usia:String
+    private lateinit var kota:String
+}
