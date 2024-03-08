532ed2a289bc5808d36a5dc2c664127fa2bf8952
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-08 12:28:27 +0300

    Added Card Stack View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     //Swipe Library
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.yuyakaido.android:card-stack-view:2.3.4")
+    implementation("com.github.yuyakaido:CardStackView:v2.3.4")
     implementation("com.squareup.picasso:picasso:2.71828")
 
     testImplementation("junit:junit:4.13.2")
