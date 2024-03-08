02c19d0bcbf8086cfd12624edfc1decc8ade349e
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-08 15:58:01 +0300

    Added CardStackAdapter Class

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/CardStackAdapter.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/CardStackAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.kotlin_spotify_random_like_app
 
-class CardStackAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CardStackAdapter : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
