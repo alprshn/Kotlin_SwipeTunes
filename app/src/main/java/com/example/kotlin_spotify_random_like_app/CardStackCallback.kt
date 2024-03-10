f8d16a2b62bfd60372ae59bf84815dbeaf5a34b5
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-10 18:40:15 +0300

    Added CardStackCallback

---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/CardStackCallback.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_spotify_random_like_app
+
+import androidx.recyclerview.widget.DiffUtil
+
+class CardStackCallback: DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNewListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
