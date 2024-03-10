97c04b9770de991bfe103bbaf86f1b39e73e95fd
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-10 18:05:24 +0300

    Edit SpotifySwipeMusic

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifySwipeMusic.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifySwipeMusic.kt
@@ -2,10 +2,17 @@ package com.example.kotlin_spotify_random_like_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackView
 
 class SpotifySwipeMusic : AppCompatActivity() {
+    private lateinit var manager:CardStackLayoutManager
+    private lateinit var adapter: CardStackAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spotify_swipe_music)
+
+        var cardStackView: CardStackView = findViewById(R.id.cardStackView)
+
     }
 }
