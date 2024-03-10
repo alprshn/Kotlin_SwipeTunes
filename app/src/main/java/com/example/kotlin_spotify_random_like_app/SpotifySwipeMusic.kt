c14965173a6f0677428177fcbdc71c4fd93f5fee
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-10 18:14:54 +0300

    Added SpotifySwipeMusic addList Function

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifySwipeMusic.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifySwipeMusic.kt
@@ -1,9 +1,16 @@
 package com.example.kotlin_spotify_random_like_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
+import com.yuyakaido.android.cardstackview.StackFrom
+import com.yuyakaido.android.cardstackview.SwipeableMethod
+
 
 class SpotifySwipeMusic : AppCompatActivity() {
     private lateinit var manager:CardStackLayoutManager
@@ -13,6 +20,28 @@ class SpotifySwipeMusic : AppCompatActivity() {
         setContentView(R.layout.activity_spotify_swipe_music)
 
         var cardStackView: CardStackView = findViewById(R.id.cardStackView)
+        manager = CardStackLayoutManager(this, CardStackListener.DEFAULT)
+        manager.setStackFrom(StackFrom.None)
+        manager.setVisibleCount(3)
+        manager.setTranslationInterval(8.0f)
+        manager.setScaleInterval(0.95f)
+        manager.setSwipeThreshold(0.3f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.FREEDOM)
+        manager.setCanScrollHorizontal(true)
+        manager.setSwipeableMethod(SwipeableMethod.Manual)
+        manager.setOverlayInterpolator(LinearInterpolator())
+        adapter = CardStackAdapter(addList())
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = adapter
+        cardStackView.itemAnimator = DefaultItemAnimator()
+    }
 
+    private fun addList(): ArrayList<ItemModel> {
+        var itemsList:ArrayList<ItemModel> = arrayListOf()
+        itemsList.add(ItemModel(R.drawable.sample1, "asd","asdad","asd"))
+        itemsList.add(ItemModel(R.drawable.sample1, "asd","asdad","asd"))
+        itemsList.add(ItemModel(R.drawable.sample1, "asd","asdad","asd"))
+        return  itemsList
     }
 }
