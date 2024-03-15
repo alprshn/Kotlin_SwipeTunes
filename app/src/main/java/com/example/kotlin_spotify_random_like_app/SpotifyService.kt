2021d4f98b5863a8b26c0e128bd36392336f3c42
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-03-15 20:08:12 +0300

    Added SpotifyService

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifyService.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/SpotifyService.kt
@@ -1,4 +1,14 @@
 package com.example.kotlin_spotify_random_like_app
 
-class SpotifyService {
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+
+interface SpotifyService {
+
+    @GET("artists/{artistId}")
+    suspend fun getArtist(
+        @Header("Authorization") authorization: String,
+        @Path("artistId") artistId: String
+    ): ArtistResponse
 }
