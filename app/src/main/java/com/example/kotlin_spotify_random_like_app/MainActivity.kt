cdc9ccda2c5fd4c5c72d34c182b951206958cfe6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-05-12 20:01:11 +0300

    Auth Edit

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.kotlin_spotify_random_like_app
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_spotify_random_like_app.databinding.ActivityMainBinding
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,6 +21,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var spotifyAuth: SpotifyConnection
     private lateinit var spotifyApi: SpotifyApi
+
+    private val clientId = "1e6d0591bbb64af286b323ff7d26ce0f"
+    private val redirectUri = "http://com.example.kotlin_spotify_random_like_app/callback"
+    private val REQUEST_CODE = 1337
 
    // private lateinit var spotifyService: SpotifyService
 
@@ -30,28 +38,15 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.button.setOnClickListener{
+            var builder : AuthorizationRequest.Builder = AuthorizationRequest.Builder(clientId, AuthorizationResponse.Type.TOKEN, redirectUri);
+            builder.setScopes(arrayOf("user-modify-playback-state"))
+            var request: AuthorizationRequest = builder.build()
+            AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
             //val intent = Intent(this, SpotifySwipeMusic::class.java)
             //startActivity(intent)
             val playlistId = "6u36F4hdBHjNi8AB38fuhf"
             val token = "Bearer BQB7yUwlOyxC3kGDvVzv_r6om6JvsflGsorUw3tZFRgEqXgkoKPnH0p-FZfd5F3P8mCwNGD3wNig_vfIKOuE1uapSTnoIMSo64ppfqaMwTgKQSEH4zg"
-            CoroutineScope(Dispatchers.IO).launch{
-                try {
-                    val pause =  spotifyApi.service.pause(token)
-                }
-                catch (e: Exception) {
-                    Log.e("deneme", "Error: ${e.message}")
-                }
-            }
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    val playlist = spotifyApi.service.getPlaylist(playlistId, token)
-                    Log.e("deneme", "Playlist Name: ${playlist.name}")
-                    Log.e("deneme", "Playlist Name: ${playlist.followers}")
-                    Log.e("deneme", "Playlist Name: ${playlist.owner.display_name}")
-                } catch (e: Exception) {
-                    Log.e("deneme", "Error: ${e.message}")
-                }
-            }
+
 
 
 
@@ -67,6 +62,30 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, data)
+
+            when (response.type) {
+                // Response was successful and contains auth token
+                AuthorizationResponse.Type.TOKEN -> {
+                    // Handle successful response
+                }
+
+                // Auth flow returned an error
+                AuthorizationResponse.Type.ERROR -> {
+                    // Handle error response
+                }
+
+                // Most likely auth flow was cancelled
+                else -> {
+                    // Handle other cases
+                }
+            }
+        }
     }
 
 }
