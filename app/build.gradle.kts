22d39e396c7decf54c9a6033b61ebefe219e65eb
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-05-26 14:59:46 +0300

    Added StartedScreenActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.yuyakaido:CardStackView:v2.3.4")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("androidx.activity:activity:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/StartedScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_spotify_random_like_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StartedScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_started_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
