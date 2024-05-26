aa3825a4ed9243df6d9f58b6a46b437c6f1c2384
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-05-26 23:02:54 +0300

    Added Splash Screen

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/SplashScreenActivity.kt
@@ -1,12 +1,17 @@
 package com.example.kotlin_spotify_random_like_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIMER: Long = 5000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,14 @@ class SplashScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val handler = Handler(Looper.getMainLooper())
+// Runnable bloğunu lambda olarak tanımlayarak 3000 ms (3 saniye) sonra çalışacak şekilde ayarlayalım.
+        handler.postDelayed({
+            val intent = Intent(this, StartedScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIMER)
     }
 }
