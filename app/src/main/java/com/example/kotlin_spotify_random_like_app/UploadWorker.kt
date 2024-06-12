fb49e7ec52b99db054c9fd58546c8ba661faafc0
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-06-12 18:46:31 +0300

    Added UploadWorker

---
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/UploadWorker.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_spotify_random_like_app
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+        // Do the work here--in this case, upload the images.
+
+
+
+        // Indicate whether the task finished successfully with the Result
+        return Result.success()
+    }
+}
