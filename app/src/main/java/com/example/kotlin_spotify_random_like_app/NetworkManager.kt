11c165321af85c4a66cc955a1258d5a2cd530526
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-02 20:52:42 +0300

    Edit: NetworkManager for Check Internet

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/NetworkManager.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/NetworkManager.kt
@@ -2,10 +2,17 @@ package com.example.kotlin_spotify_random_like_app
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import androidx.lifecycle.LiveData
 
 class NetworkManager(contex:Context): LiveData<Boolean>() {
     private var connectivityManager = contex.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
+    private val networkCallback = object : ConnectivityManager.NetworkCallback(){
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+        }
 
+
+    }
 }
