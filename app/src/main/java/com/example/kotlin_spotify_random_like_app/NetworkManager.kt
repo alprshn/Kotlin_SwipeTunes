19425fe4d6dba2c37431994da0dd260407ffdd4b
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-02 20:54:56 +0300

    Edit: NetworkManager for Check Internet

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/NetworkManager.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/NetworkManager.kt
@@ -11,8 +11,18 @@ class NetworkManager(contex:Context): LiveData<Boolean>() {
     private val networkCallback = object : ConnectivityManager.NetworkCallback(){
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
+            postValue(true)
         }
 
+        override fun onUnavailable() {
+            super.onUnavailable()
+            postValue(false)
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            postValue(false)
+        }
 
     }
 }
