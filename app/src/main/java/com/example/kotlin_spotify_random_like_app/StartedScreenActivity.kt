353d16055624f63372acfb17e7497f7667347fe2
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-04 23:46:19 +0300

    Added: isSpotifyInstalled

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/StartedScreenActivity.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/StartedScreenActivity.kt
@@ -16,6 +16,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.spotify.sdk.android.auth.AuthorizationClient
@@ -51,6 +52,7 @@ class StartedScreenActivity : AppCompatActivity() {
         networkManager = NetworkManager(this)
 
         setupSpotifyConnection()
+        checkIsSpotifyInstalled()
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         viewPager = findViewById(R.id.slider)
@@ -76,11 +78,19 @@ class StartedScreenActivity : AppCompatActivity() {
     }
 
     private fun checkIsSpotifyInstalled(){
-        if (spotifyAuth.checkSpotifyInstalled(this)){
+        val spotifyInstalledLiveData = SpotifyInstalledLiveData(this)
 
-        }else{
+        spotifyInstalledLiveData.observe(this, Observer { isInstalled ->
+            if (isInstalled) {
+                // Spotify uygulaması yüklü
+                Log.e("Spotify uygulaması yüklü.","Spotify uygulaması yüklü")
+            } else {
+                // Spotify uygulaması yüklü değil
+                Log.e("Spotify uygulaması yüklü değil.","Spotify uygulaması yüklü değil")
+                //println("Spotify uygulaması yüklü değil.")
+            }
+        })
 
-        }
     }
     private fun checkFirstTimeLaunch() {
         val sharedPref = getSharedPreferences(prefsName, MODE_PRIVATE)
