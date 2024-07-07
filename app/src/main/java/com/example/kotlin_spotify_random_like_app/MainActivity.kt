cdc8528ca3456450db16ed41be049c52668bc0a0
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-07 22:46:34 +0300

    Edit: Main Activity

--- app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_spotify_random_like_app/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         saveAccessToken()
         setupButtons()
         Log.e("denemeRefreshToken", SpotifyApiManager.refreshToken.toString())
-        CircularMenu()
+        circularMenu()
 
     }
     private fun setupButtons() {
