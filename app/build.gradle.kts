2ca4e42872a2991c263aec051d150d91934d79df
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-08 08:30:59 +0300

    Edit: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,10 +73,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    //implementation(project(":libs"))
-    //I added dependencies with this link https://developer.android.com/studio/projects/android-library#kts
-    //implementation(project("libs/spotify-app.aar"))
-    //implementation(files("libs/spotify-auth-release-2.1.0.aar"))
 
 // Kotlin + coroutines
     val work_version = "2.9.0"
@@ -96,13 +92,15 @@ dependencies {
 
     implementation("com.github.bumptech.glide:glide:4.14.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.1")
-
+    //Button Library
     implementation("com.github.SanojPunchihewa:GlowButton:1.0.1")
+    implementation ("com.ramotion.circlemenu:circle-menu:0.3.2")
 
+    //Lottie
     val lottieVersion = "3.4.0"
     implementation ("com.airbnb.android:lottie:$lottieVersion")
 
-    implementation ("com.ramotion.circlemenu:circle-menu:0.3.2")
+
 
 
 }
